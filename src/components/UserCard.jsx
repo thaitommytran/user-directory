31bdbb494b4badc5e68ce06e66fe281f41a0bba3
Thai Tran <thai.tommy.tran@gmail.com>
2022-07-15 13:57:56 -0500

    added nav button functionality

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,46 +4,73 @@ import "../styles/UserCard.css";
 
 const UserCard = () => {
   const [index, setIndex] = useState(0);
+  let user = data[index];
+
+  const next = () => {
+    if (index === data.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const previous = () => {
+    if (index === 0) {
+      setIndex(data.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
 
   return (
     <div id="background">
       <div id="card-main">
-        <h2 id="count">1/25</h2>
+        <h2 id="count">
+          {user.id}/{data.length}
+        </h2>
         <div id="card-data">
           <span>
-            <h1 id="name">userFirstName userLastName</h1>
+            <h1 id="name">
+              {user.name.first} {user.name.last}
+            </h1>
             <div id="line"></div>
           </span>
           <div id="details">
             <div className="info-container">
               <h3 className="user-info">From:</h3>
-              <p>userCity, userCountry</p>
+              <p>
+                {user.city}, {user.country}
+              </p>
             </div>
             <div className="info-container">
               <h3 className="user-info">Job Title:</h3>
-              <p>userTitle</p>
+              <p>{user.title}</p>
             </div>
             <div className="info-container">
               <h3 className="user-info">Employer:</h3>
-              <p>userEmployer</p>
+              <p>{user.employer}</p>
             </div>
           </div>
           <div id="movies">
             <h3 className="user-info">Favorite Movies:</h3>
-            <p className="movie">1. userFavoriteMovies</p>
-            <p className="movie">2. userFavoriteMovies</p>
-            <p className="movie">3. userFavoriteMovies</p>
+            <p className="movie">1. {user.favoriteMovies[0]}</p>
+            <p className="movie">2. {user.favoriteMovies[1]}</p>
+            <p className="movie">3. {user.favoriteMovies[2]}</p>
           </div>
         </div>
       </div>
       <div className="button-container">
-        <button className="nav-button">{"<"} Previous</button>
+        <button className="nav-button" onClick={previous}>
+          {"<"} Previous
+        </button>
         <div id="edit-container">
           <button className="info-button">Edit</button>
           <button className="info-button">Delete</button>
           <button className="info-button">New</button>
         </div>
-        <button className="nav-button">Next {">"}</button>
+        <button className="nav-button" onClick={next}>
+          Next {">"}
+        </button>
       </div>
     </div>
   );
